7c0b67cf0f1dd34277a9b590d2d826c1614f5140
Alibi <yoooholly71@gmail.com>
2024-03-24 20:26:15 +0100

    modified file

--- Level04/sort_int_tab/sort_int_tab.c
+++ Level04/sort_int_tab/sort_int_tab.c
@@ -1,1 +1,23 @@
+void ft_swap(int *a, int *b)
+{
+    int temp = *a;
+    *a = *b;
+    *b = temp;
+}
 
+void sort_int_tab(int *tab, unsigned int size)
+{
+    int i = 0;
+
+    while (size > 0)
+    {
+        i = 0;
+        while (i < size - 1)
+        {
+            if (tab[i] > tab[i + 1])
+                ft_swap(&tab[i], &tab[i + 1]);
+            i++;
+        }
+        size--;
+    }
+}
