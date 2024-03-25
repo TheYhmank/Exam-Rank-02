d1ca649b0b2ac9231ac3da5205eef547c03fea0f
Alibi <yoooholly71@gmail.com>
2024-03-25 20:57:04 +0100

    modified file

--- Level04/rostring/rostring.c
+++ Level04/rostring/rostring.c
@@ -38,14 +38,25 @@ int main(int argc, char *argv[])
 {
     int i;
     char *fix_str;
+    char *start;
+    char *end;
 
     if (argc == 2)
     {
         i = 0;
         fix_str = single_space(argv[1]);
-        
+        while (fix_str[i] && !(fix_str[i] == ' ' || fix_str[i] == '\t'))
+            i++;
+        while (fix_str[++i])
+            write(1, &fix_str[i], 1);
+        i = 0;
+        write(1, " ", 1);
+        while (fix_str[i] && !(fix_str[i] == ' ' || fix_str[i] == '\t'))
+            write(1, &fix_str[i++], 1);
         free(fix_str);
     }
     write(1, "\n", 1);
     return 0;
 }
+
+//finish it!
