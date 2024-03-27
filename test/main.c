ed39b1c167a3f8a8d6fc833ba94d01871e5f687e
Alibi <yoooholly71@gmail.com>
2024-03-27 20:25:56 +0100

    new files

--- test/main.c
+++ test/main.c
@@ -7,87 +7,15 @@ typedef struct Node
 	struct Node* next;
 } Node;
 
-void insert_end(Node** root, int src)
-{
-	Node* new_node = malloc(sizeof(Node));
-	if (!(new_node))
-		exit(2);
-
-	new_node->next = NULL;
-	new_node->x = src;
-
-	if (*root == NULL)
-	{
-		root = &new_node;
-		return;
-	}
-	
-	Node* curr = *root;
-	while (curr->next)
-		curr = curr->next;
-	curr->next = new_node;	
-}
-
 void deallocate(Node** root)
 {
-
-} //do this by your own
-
-int main(int argc, char const *argv[])
-{
-	Node* root = malloc(sizeof(Node));
-	root->next = NULL;
-	root->x = 15;
-
-	insert_end(&root, 23);
-
-	Node* curr = root;
+	Node* curr = *root;
 	while (curr)
 	{
-		printf("%d\n", curr->x);
+		Node* aux = curr;
 		curr = curr->next;
+		free(aux);
 	}
-	return (0);
+	*root = NULL;
 }
 
-
-/* V02
-int main(int argc, char const *argv[])
-{
-	Node root;
-	root.x = 15;
-	root.next = malloc(sizeof(Node));
-	root.next->x = -8;
-	root.next->next = malloc(sizeof(Node));
-	root.next->next->x = -2;
-	root.next->next->next = NULL;
-
-	Node* curr = &root;
-	while (curr)
-	{
-		printf("%d\n", curr->x);
-		curr = curr->next;
-	}
-
-	free(root.next->next);
-	free(root.next);
-	return 0;
-}
-*/
-
-/* V01
-int main(int argc, char const *argv[])
-{
-	Node root;
-	root.x = 15;
-	root.next = malloc(sizeof(Node));
-	root.next->x = -1;
-	root.next->next = NULL;
-
-	printf("First Node element: %d\n", root.x);
-	printf("Second Node element: %d", root.next->x);
-
-	free(root.next);
-	return 0;
-}
-*/
