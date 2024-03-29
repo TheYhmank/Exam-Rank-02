1f66082c742586bf1677386f14177cffad258f10
Alibi <yoooholly71@gmail.com>
2024-03-29 11:46:56 +0100

    modified files

--- Level03/pgcd/pgcd.c
+++ Level03/pgcd/pgcd.c
@@ -3,7 +3,13 @@
 
 int pgcd(int num1, int num2)
 {
-    
+    while (num2)
+    {
+        int temp = num2;
+        num2 = num1 % num2;
+        num1 = temp;
+    }
+    return (num1);
 }
 
 int main(int argc, char const *argv[])
